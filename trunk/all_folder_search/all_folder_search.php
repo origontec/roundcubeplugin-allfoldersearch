3568207afb0e5c8800b78c64c5ef614a0496eb18
stephanejourney@gmail.com <stephanejourney@gmail.com@9e117758-7be8-11de-a7be-13e53affbef8>
2013-06-20 22:25:38 +0000

    Working with Roundcube 0.9, BETA release.

--- trunk/all_folder_search/all_folder_search.php
+++ trunk/all_folder_search/all_folder_search.php
@@ -61,22 +61,25 @@ class all_folder_search extends rcube_plugin
      */
     function search_override($args)
     {
+	global $RCMAIL;
         // return if not an all folder search
-        if(strpos(get_input_value('_headers', RCUBE_INPUT_GET), 'all_folder_search') === false)
+	//BUG
+        if(strpos(get_input_value('_headers', RCUBE_INPUT_GET), 'all_folder_search') === FALSE)
         {
             // use roundcube's search.inc
             $args['abort'] = false;
         }
         else
         {
-            global $IMAP, $OUTPUT;
-
             // reset list_page and old search results
-            $IMAP->set_page(1);
-            $IMAP->set_search_set(NULL);
+		
+	     //BUG Original
+            $RCMAIL->imap->set_page(1);
+            $RCMAIL->imap->set_search_set(NULL);
             $_SESSION['page'] = 1;
             $page = get_input_value('_page', RCUBE_INPUT_GET);
             $page = $page ? $page : 1;
+	    
 
             // get search string
             $str = get_input_value('_q', RCUBE_INPUT_GET);
@@ -147,17 +150,18 @@ class all_folder_search extends rcube_plugin
             $search_str = trim($search_str);
             $count = 0;
             $result_h = Array();
-            $tmp_page_size = $IMAP->page_size;
-            $IMAP->page_size = 500;
+            $tmp_page_size = $RCMAIL->imap->page_size;
+            $RCMAIL->imap->set_pagesize(500);
 
             if($use_saved_list && $_SESSION['all_folder_search']['uid_mboxes'])
                 $result_h = $this->get_search_result();
-            else
+            else{
                 $result_h = $this->perform_search($search_str);
+	    }
 
-            $OUTPUT->set_env('all_folder_search_active', 1);
+            $RCMAIL->output->set_env('all_folder_search_active', 1);
 
-            $IMAP->page_size = $tmp_page_size;
+            $RCMAIL->imap->set_pagesize($tmp_page_size);
             $count = count($result_h);
 
             $this->sort_search_result($result_h);
@@ -167,24 +171,27 @@ class all_folder_search extends rcube_plugin
             // Make sure we got the headers
             if (!empty($result_h))
             {
-              rcmail_js_message_list($result_h);
+              rcmail_js_message_list($result_h, false , false);
+
               if ($search_str)
-                $OUTPUT->show_message('searchsuccessful', 'confirmation', array('nr' => $count));
+                $RCMAIL->output->show_message('searchsuccessful', 'confirmation', array('nr' => $count));
+
+	      $RCMAIL->output->command('list_mailbox');
             }
             else
             {
-              $OUTPUT->show_message('searchnomatch', 'notice');
+              $RCMAIL->output->show_message('searchnomatch', 'notice');
             }
 
             // update message count display
-            $OUTPUT->set_env('search_request', "ALLFOLDERSSEARCHREQ");
-            $OUTPUT->set_env('messagecount', $count);
-            $OUTPUT->set_env('pagecount', ceil($count/$IMAP->page_size));
-            $OUTPUT->command('set_rowcount', rcmail_get_messagecount_text($count, $page));
-            $OUTPUT->send();
+            $RCMAIL->output->set_env('search_request', "ALLFOLDERSSEARCHREQ");
+            $RCMAIL->output->set_env('messagecount', $count);
+            $RCMAIL->output->set_env('pagecount', ceil($count/$RCMAIL->imap->page_size));
+            $RCMAIL->output->command('set_rowcount', rcmail_get_messagecount_text($count, $page));
 
             // dont execute roundcube's search.inc
             $args['abort'] = true;
+            $RCMAIL->output->send();
         }
 
         return $args;
@@ -197,7 +204,7 @@ class all_folder_search extends rcube_plugin
      */
     function list_override($args)
     {
-        global $OUTPUT, $CONFIG;
+        global $RCMAIL;
 
         // return if not an all folder search
         //   cant check using session here because not_all_folder_search
@@ -238,8 +245,8 @@ class all_folder_search extends rcube_plugin
             $result_h = $this->get_paged_result($result_h, $page);
             rcmail_js_message_list($result_h);
 
-            $OUTPUT->command('set_rowcount', rcmail_get_messagecount_text($count, $page));
-            $OUTPUT->send();
+            $RCMAIL->output->command('set_rowcount', rcmail_get_messagecount_text($count, $page));
+            $RCMAIL->output->send();
 
             // dont execute roundcube's list.inc
             $args['abort'] = true;
@@ -255,7 +262,7 @@ class all_folder_search extends rcube_plugin
      */
     function mark_override($args)
     {
-        global $OUTPUT, $IMAP, $CONFIG;
+        global $RCMAIL;
 
         // return if not an all folder search
         if(!$_SESSION['all_folder_search']['uid_mboxes'])
@@ -272,7 +279,7 @@ class all_folder_search extends rcube_plugin
             {
                 // count messages before changing anything
                 $old_count = count($_SESSION['all_folder_search']['uid_mboxes']);
-                $old_pages = ceil($old_count / $IMAP->page_size);
+                $old_pages = ceil($old_count / $RCMAIL->imap->page_size);
                 $count = sizeof(explode(',', $uids));
             }
 
@@ -282,17 +289,17 @@ class all_folder_search extends rcube_plugin
             foreach($uids as $uid)
             {
                 $mbox = $_SESSION['all_folder_search']['uid_mboxes'][$uid]['mbox'];
-                $IMAP->set_mailbox($mbox);
-                $marked = $IMAP->set_flag($_SESSION['all_folder_search']['uid_mboxes'][$uid]['uid'], $flag);
+                $RCMAIL->imap->set_mailbox($mbox);
+                $marked = $RCMAIL->imap->set_flag($_SESSION['all_folder_search']['uid_mboxes'][$uid]['uid'], $flag);
 
                 if ($marked == -1)
                 {
                     // send error message
                     if ($_POST['_from'] != 'show')
-                        $OUTPUT->command('list_mailbox');
+                        $RCMAIL->output->command('list_mailbox');
 
-                    $OUTPUT->show_message('errormarking', 'error');
-                    $OUTPUT->send();
+                    $RCMAIL->output->show_message('errormarking', 'error');
+                    $RCMAIL->output->send();
                     return $args;
                 }
             }
@@ -305,64 +312,65 @@ class all_folder_search extends rcube_plugin
                 foreach($uids as $uid)
                 {
                     $mbox = $_SESSION['all_folder_search']['uid_mboxes'][$uid]['mbox'];
-                    $IMAP->set_mailbox($mbox);
-                    $read = $IMAP->set_flag($_SESSION['all_folder_search']['uid_mboxes'][$uid]['uid'], 'SEEN');
+                    $RCMAIL->imap->set_mailbox($mbox);
+                    $read = $RCMAIL->imap->set_flag($_SESSION['all_folder_search']['uid_mboxes'][$uid]['uid'], 'SEEN');
 
                     if ($read != -1 && !$CONFIG['skip_deleted'])
-                        $OUTPUT->command('flag_deleted_as_read', $uid);
+                        $RCMAIL->output->command('flag_deleted_as_read', $uid);
                 }
             }
 
             if ($flag == 'SEEN' || $flag == 'UNSEEN' || ($flag == 'DELETED' && !$CONFIG['skip_deleted']))
             {
                 // just update unread count for all mailboxes, easier than figuring out which were changed
-                $mbox_names = $IMAP->list_mailboxes();
+                $mbox_names = $RCMAIL->imap->list_mailboxes();
 
                 foreach($mbox_names as $mbox)
-                    $OUTPUT->command('set_unread_count', $mbox, $IMAP->messagecount($mbox, 'UNSEEN'), ($mbox == 'INBOX'));
+                    $RCMAIL->output->command('set_unread_count', $mbox, $RCMAIL->imap->messagecount($mbox, 'UNSEEN'), ($mbox == 'INBOX'));
             }
             else if ($flag == 'DELETED' && $CONFIG['skip_deleted'])
             {
                 if ($_POST['_from'] == 'show')
                 {
                     if ($next = get_input_value('_next_uid', RCUBE_INPUT_GPC))
-                        $OUTPUT->command('show_message', $next);
+                        $RCMAIL->output->command('show_message', $next);
                     else
-                        $OUTPUT->command('command', 'list');
+                        $RCMAIL->output->command('command', 'list');
                 }
                 else
                 {
+
                     // refresh saved search set after moving some messages
                     if (($search_request = get_input_value('_search', RCUBE_INPUT_GPC)) && $_SESSION['all_folder_search']['uid_mboxes'])
                     {
-                        $_SESSION['search'][$search_request] = $this->perform_search($IMAP->search_string);
+                        $_SESSION['search'][$search_request] = $this->perform_search($RCMAIL->imap->search_string);
                     }
 
                     $msg_count      = count($_SESSION['all_folder_search']['uid_mboxes']);
-                    $pages          = ceil($msg_count / $IMAP->page_size);
-                    $nextpage_count = $old_count - $IMAP->page_size * $_SESSION['page'];
-                    $remaining      = $msg_count - $IMAP->page_size * ($_SESSION['page'] - 1);
+                    $pages          = ceil($msg_count / $RCMAIL->imap->page_size);
+                    $nextpage_count = $old_count - $RCMAIL->imap->page_size * $_SESSION['page'];
+                    $remaining      = $msg_count - $RCMAIL->imap->page_size * ($_SESSION['page'] - 1);
 
                     // jump back one page (user removed the whole last page)
                     if ($_SESSION['page'] > 1 && $nextpage_count <= 0 && $remaining == 0)
                     {
-                        $IMAP->set_page($_SESSION['page']-1);
-                        $_SESSION['page'] = $IMAP->list_page;
+                        $RCMAIL->imap->set_page($_SESSION['page']-1);
+                        $_SESSION['page'] = $RCMAIL->imap->list_page;
                         $jump_back = true;
                     }
 
                     // update message count display
-                    $OUTPUT->set_env('messagecount', $msg_count);
-                    $OUTPUT->set_env('current_page', $IMAP->list_page);
-                    $OUTPUT->set_env('pagecount', $pages);
+                    $RCMAIL->output->set_env('messagecount', $msg_count);
+                    $RCMAIL->output->set_env('current_page', $RCMAIL->imap->list_page);
+                    $RCMAIL->output->set_env('pagecount', $pages);
 
                     // update mailboxlist
-                    foreach($IMAP->list_mailboxes() as $mbox)
+                    foreach($RCMAIL->imap->list_mailboxes() as $mbox)
                     {
-                        $unseen_count = $msg_count ? $IMAP->messagecount($mbox, 'UNSEEN') : 0;
-                        $OUTPUT->command('set_unread_count', $mbox, $unseen_count, ($mbox == 'INBOX'));
+                        $unseen_count = $msg_count ? $RCMAIL->imap->messagecount($mbox, 'UNSEEN') : 0;
+                        $RCMAIL->output->command('set_unread_count', $mbox, $unseen_count, ($mbox == 'INBOX'));
                     }
-                    $OUTPUT->command('set_rowcount', rcmail_get_messagecount_text($msg_count));
+                    $RCMAIL->output->command('set_rowcount', rcmail_get_messagecount_text($msg_count));
 
                     // add new rows from next page (if any)
                     if (($jump_back || $nextpage_count > 0))
@@ -380,7 +388,7 @@ class all_folder_search extends rcube_plugin
                 }
             }
 
-            $OUTPUT->send();
+            $RCMAIL->output->send();
 
             // dont execute roundcube's mark.inc
             $args['abort'] = true;
@@ -396,7 +404,7 @@ class all_folder_search extends rcube_plugin
      */
     function mov_del_override($args)
     {
-        global $IMAP, $OUTPUT, $CONFIG;
+        global $RCMAIL;
 
         // return if not an all folder search
         if(!$_SESSION['all_folder_search']['uid_mboxes'])
@@ -415,31 +423,31 @@ class all_folder_search extends rcube_plugin
                 $count = sizeof(explode(',', ($uids = get_input_value('_uid', RCUBE_INPUT_POST))));
                 $target = get_input_value('_target_mbox', RCUBE_INPUT_POST);
                 $mbox = $_SESSION['all_folder_search']['uid_mboxes'][$uids[0]];
-                $IMAP->set_mailbox($mbox);
+                $RCMAIL->imap->set_mailbox($mbox);
 
                 // flag messages as read before moving them
                 if ($CONFIG['read_when_deleted'] && $target == $CONFIG['trash_mbox'])
-                    $IMAP->set_flag($uids, 'SEEN');
+                    $RCMAIL->imap->set_flag($uids, 'SEEN');
 
 console("moving $uids from $mbox to $target");
-                $moved = $IMAP->move_message($uids, $target, $mbox);
+                $moved = $RCMAIL->imap->move_message($uids, $target, $mbox);
 
                 if (!$moved) {
                     // send error message
                 if ($_POST['_from'] != 'show')
-                      $OUTPUT->command('list_mailbox');
-                    $OUTPUT->show_message('errormoving', 'error');
-                    $OUTPUT->send();
+                      $RCMAIL->output->command('list_mailbox');
+                    $RCMAIL->output->show_message('errormoving', 'error');
+                    $RCMAIL->output->send();
                     exit;
                 }
                 else
-                    $OUTPUT->show_message("Successfully moved message from $mbox to $target");
+                    $RCMAIL->output->show_message("Successfully moved message from $mbox to $target");
 
                 $addrows = true;
             }
 
             // send response
-            $OUTPUT->send();
+            $RCMAIL->output->send();
 
             // dont execute roundcube's mov_del.inc
             $args['abort'] = true;
@@ -469,21 +477,22 @@ console("moving $uids from $mbox to $target");
      */
     function perform_search($search_string)
     {
-        global $IMAP, $OUTPUT;
+        global $RCMAIL;
         $result_h = Array();
         $uid_mboxes = Array();
         $id = 1;
 
         // Search all folders and build a final set
-        foreach($IMAP->list_mailboxes() as $mbox)
+        foreach($RCMAIL->imap->list_mailboxes() as $mbox)
         {
             // dont include junk or trash in the all folder search results
             if($mbox == $CONFIG['junk_mbox'] || $mbox == $CONFIG['trash_mbox'])
                 continue;
 
-            $IMAP->set_mailbox($mbox);
-            $IMAP->search($mbox, $search_string, RCMAIL_CHARSET, $_SESSION['sort_col']);
-            $result = $IMAP->list_headers($mbox, 1, $_SESSION['sort_col'], $_SESSION['sort_order']);
+            $RCMAIL->imap->set_mailbox($mbox);
+            $RCMAIL->imap->search($mbox, $search_string, RCMAIL_CHARSET, $_SESSION['sort_col']);
+            //$result = $RCMAIL->imap->list_headers($mbox, 1, $_SESSION['sort_col'], $_SESSION['sort_order']);
+	    $result = $RCMAIL->imap->list_messages($mbox, 1, $_SESSION['sort_col'], $_SESSION['sort_order']);
 
             foreach($result as $row)
             {
@@ -491,13 +500,13 @@ console("moving $uids from $mbox to $target");
 
                 $row->uid = $id;
                 $result_h[] = $row;
-
+                //array_push($result_h, $row);
                 $id++;
             }
         }
 
         $_SESSION['all_folder_search']['uid_mboxes'] = $uid_mboxes;
-        $OUTPUT->set_env('all_folder_search_uid_mboxes', $uid_mboxes);
+        $RCMAIL->output->set_env('all_folder_search_uid_mboxes', $uid_mboxes);
 
         return $result_h;
     }
@@ -510,7 +519,7 @@ console("moving $uids from $mbox to $target");
      */
     function get_search_result()
     {
-        global $IMAP;
+	global $RCMAIL;
         $result_h = array();
 
         foreach($_SESSION['all_folder_search']['uid_mboxes'] as $id => $uid_mbox)
@@ -519,8 +528,8 @@ console("moving $uids from $mbox to $target");
             $mbox = $uid_mbox['mbox'];
 
             // get the message headers directly, there is a caching issue with the imap wrapper
-            //$IMAP->get_headers($uid, $mbox);
-            $row = iil_C_FetchHeader($IMAP->conn, $mbox, $uid, TRUE, FALSE, $IMAP->fetch_add_headers);
+            $row = $RCMAIL->imap->get_headers($uid, $mbox);
+            //$row = $RCMAIL->imap->iil_C_FetchHeader($RCMAIL->imap->conn, $mbox, $uid, TRUE, FALSE, $RCMAIL->imap->fetch_add_headers);
             $row->uid = $id;
 
             array_push($result_h, $row);
@@ -539,11 +548,11 @@ console("moving $uids from $mbox to $target");
      */
     function get_paged_result($result_h, $page)
     {
-        global $IMAP;
+        global $RCMAIL;
 
         // Apply page size rules
-        if(count($result_h) > $IMAP->page_size)
-            $result_h = array_slice($result_h, ($page-1)*$IMAP->page_size, $IMAP->page_size);
+        if(count($result_h) > $RCMAIL->imap->page_size)
+            $result_h = array_slice($result_h, ($page-1)*$RCMAIL->imap->page_size, $RCMAIL->imap->page_size);
 
         return $result_h;
     }
@@ -569,7 +578,7 @@ console("moving $uids from $mbox to $target");
                                 $second = $result_h[$y]->timestamp;
                                 break;
                 case 'size':    $first = $result_h[$x]->size;
-                                $second = $result_h[$y]->size;
+                                $iecond = $result_h[$y]->size;
                                 break;
                 case 'subject': $first = strtolower($result_h[$x]->subject);
                                 $second = strtolower($result_h[$y]->subject);
